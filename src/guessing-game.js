cda1d2f11b5f33617056cdd046dd9d4eef802ba7
SergeySkakun <thexfiles116@gmail.com>
2017-09-22 20:59:51 +0300

    guessing-game/feature: create methods of class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        return this;
     }
 
     guess() {
-
+        this.avg = this.min + Math.round((this.max - this.min)/2);
+        return this.avg;
     }
 
     lower() {
-
+        this.max = this.avg;
     }
 
     greater() {
-
+        this.min = this.avg;
     }
 }
 
